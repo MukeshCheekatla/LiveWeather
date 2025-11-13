9c9d0119479599f657e750ce1d11d98dad310c64
Sai Mukesh Cheekatla <cheekatlamukesh@gmail.com>
2025-11-13 17:57:49 +0530

    fetching data from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 function App() {
   const [city, setCity] = useState("");
 
+  async function getWeather() {
+    const res = await fetch(`http://localhost:5000/weather?city=${city}`);
+    const data = await res.json();
+    console.log(data); // TEMP step
+  }
+
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-5">
       <div className="bg-white shadow-xl rounded-xl p-8 max-w-md w-full">
@@ -19,7 +25,10 @@ function App() {
             className="w-full px-4 py-2 border rounded-lg focus:outline-none"
           />
 
-          <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+          <button
+            onClick={getWeather}
+            className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+          >
             Search
           </button>
         </div>
