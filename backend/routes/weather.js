41587f8c50e09eba03e34e730d793c2b6e90f05a
Sai Mukesh Cheekatla <cheekatlamukesh@gmail.com>
2025-11-13 16:29:03 +0530

    Add CORS

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -3,30 +3,42 @@ import axios from "axios";
 
 const router = express.Router();
 
-// GET: /weather?city=London
 router.get("/", async (req, res) => {
   try {
     const city = req.query.city;
 
     if (!city) {
-      return res.status(400).json({ error: "City name is required" });
+      return res.status(400).json({
+        success: false,
+        message: "City name is required",
+      });
     }
 
     const apiKey = process.env.WEATHER_API_KEY;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     const response = await axios.get(url);
+    const data = response.data;
 
     res.json({
-      city: response.data.name,
-      temperature: response.data.main.temp,
-      weather: response.data.weather[0].main,
-      description: response.data.weather[0].description,
-      humidity: response.data.main.humidity,
-      wind: response.data.wind.speed,
+      success: true,
+      data: {
+        city: data.name,
+        temp: data.main.temp,
+        min: data.main.temp_min,
+        max: data.main.temp_max,
+        humidity: data.main.humidity,
+        wind: data.wind.speed,
+        condition: data.weather[0].main,
+        description: data.weather[0].description,
+        icon: data.weather[0].icon,
+      },
     });
   } catch (error) {
-    res.status(500).json({ error: "Unable to fetch weather" });
+    res.status(500).json({
+      success: false,
+      message: "Unable to fetch weather",
+    });
   }
 });
 
