5d1641f9363ced98538542d4eef08a8ce8269e10
Sai Mukesh Cheekatla <cheekatlamukesh@gmail.com>
2025-11-13 16:51:16 +0530

    adding config file, updating weatherservice

---
+++ backend/config/apiConfig.js
@@ -0,0 +1,4 @@
+export const apiConfig = {
+  baseUrl: "https://api.openweathermap.org/data/2.5/weather",
+  units: "metric",
+};
--- backend/services/weatherService.js
+++ backend/services/weatherService.js
@@ -1,16 +1,20 @@
 import axios from "axios";
+import { apiConfig } from "../config/apiConfig.js";
 
 export async function getWeatherData(city) {
   if (!city) throw new Error("City is required");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
+  const url = `${apiConfig.baseUrl}?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=${apiConfig.units}`;
 
   const res = await axios.get(url);
+
   return {
     city: res.data.name,
     temp: res.data.main.temp,
     humidity: res.data.main.humidity,
     wind: res.data.wind.speed,
     condition: res.data.weather[0].main,
+    description: res.data.weather[0].description,
+    icon: res.data.weather[0].icon,
   };
 }
