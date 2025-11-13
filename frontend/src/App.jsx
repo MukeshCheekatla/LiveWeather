b9f13cd852fd8ac0643d7712462a75405f628993
Sai Mukesh Cheekatla <cheekatlamukesh@gmail.com>
2025-11-13 17:51:03 +0530

    Create Weather Search UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
 function App() {
+  const [city, setCity] = useState("");
+
   return (
-    <div className="p-10">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind is Working
-      </h1>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-5">
+      <div className="bg-white shadow-xl rounded-xl p-8 max-w-md w-full">
+        <h1 className="text-3xl font-bold text-center mb-6 text-blue-600">
+          Live Weather App
+        </h1>
+
+        <div className="flex gap-3">
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Enter city"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none"
+          />
+
+          <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+            Search
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
